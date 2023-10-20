a602eee3b3b0c49ff2ba067c74f72ac168a00068
Sagar <sagarsati9@gmail.com>
2023-10-20 13:34:00 +0530

    GPT Search Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { addUser,removeUser } from '../utils/userSlice';
 import { LOGO } from '../utils/constants';
+import { toggleGptSearchView } from '../utils/gptSlice';
 
 const Header = () => {
 
@@ -40,6 +41,11 @@ const Header = () => {
         return () => unsubscribe();
       },[]);
   
+    const handleGptSearchClick = () => {
+      // Toggle GPT Search
+      dispatch(toggleGptSearchView());
+    };
+
 
     return (
         <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
@@ -49,7 +55,10 @@ const Header = () => {
                 alt='logo'
             />
              
-           { user &&( <div className='flex p-2'>
+           { user &&( 
+            <div className='flex p-2'>
+              <button className='py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg'
+              onClick={handleGptSearchClick}>GPT Search</button>
                 <img
                  className='w-12 h-12'
                     alt="usericon"
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,3 +10,5 @@ export const API_OPTIONS =  {
   };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
+export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/f85718e8-fc6d-4954-bca0-f5eaf78e0842/ea44b42b-ba19-4f35-ad27-45090e34a897/IN-en-20230918-popsignuptwoweeks-perspective_alpha_website_small.jpg";
